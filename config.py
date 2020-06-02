143fd57ae7a8de68e8923cdc67ced677a49df15f
Pavel Malinkin <pavmalin@mail.ru>
2020-06-02 21:02:57 +0300

    config database_url

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-db_path = 'postgresql://postgres:pass@127.0.0.1:5432/cookbook'
+#db_path = 'postgresql://postgres:pass@127.0.0.1:5432/cookbook'
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = db_path
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SQLALCHEMY_DATABASE_URI = db_path
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
